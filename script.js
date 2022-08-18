618bbb537d8c76919b611ff8e14351b9e1bccf6c
andriali044 <andriali044gmail.com>
2022-08-18 11:42:28 +0700

    first codet

---
+++ script.js
@@ -0,0 +1,25 @@
+var bisa = parseInt(prompt('masukkan Run 1 :'));
+if( bisa === Run1 ){
+    alert('yes');
+} else if (bisa === Run2 ) {
+    alert('Ok');
+} else if (bisa === Run3 ) {
+    alert('ok');
+} else if (bisa === Run4 ) {
+    alert('ok');
+}
+
+var sabi = parseInt(prompt('masukkan Run 1 :'));
+if( sabi === Run1Err ){
+    alert('error');
+} else if (sabi === Run4Err ) {
+    alert('error');
+} else if (sabi === Run3Err ) {
+    alert('error');
+} else if (sabi === Run2Err ) {
+    alert('error');
+}
+
+
+
+
